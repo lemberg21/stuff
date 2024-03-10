b1a6a35856f7cdaefd1654ee7435a5ba7b79e7ed
Volodymyr Khomyshyn <121640560+lemberg21@users.noreply.github.com>
2024-03-10 19:47:35 +0200

    Update progressbar.ps1

--- microservices/jenkins/progressbar.ps1
+++ microservices/jenkins/progressbar.ps1
@@ -1,4 +1,6 @@
-for($ = 1; $i -le 100; $i++ ){
-    Write-Progress -Activity "Action in Progress" -Status "$i% Complete:" -percentCmplete $i
-    Start-Sleep -Miliseconds 250
+fuction ProgressBarr {
+ for($ = 1; $i -le 100; $i++ ){
+     Write-Progress -Activity "Action in Progress" -Status "$i% Complete:" -percentCmplete $i
+     Start-Sleep -Miliseconds 250 
+ }
 }
