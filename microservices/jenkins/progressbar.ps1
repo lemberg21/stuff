8b786799f0446df75c9513d8b3ac1bbe688e3edc
Volodymyr Khomyshyn <121640560+lemberg21@users.noreply.github.com>
2024-08-26 11:36:05 +0300

    Delete microservices/jenkins directory

--- microservices/jenkins/progressbar.ps1
+++
@@ -1,6 +0,0 @@
-fuction ProgressBarr {
- for($ = 1; $i -le 100; $i++ ){
-     Write-Progress -Activity "Action in Progress" -Status "$i% Complete:" -percentCmplete $i
-     Start-Sleep -Miliseconds 250 
- }
-}
