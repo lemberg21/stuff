8675b8ab962cd5d043d291c2fd8bb3f0922fa155
vok1 <lemberg_ua@yahoo.com>
2024-08-30 11:07:26 +0300

    fix bugs

--- db-project/ansibleInstall.sh
+++ db-project/ansibleInstall.sh
@@ -5,3 +5,6 @@ sudo apt upgrade -y
 sudo apt-add-repository ppa: ansible/ansible
 sudo apt install ansible git -y
 ansible --version
+sudo mkdir /etc/ansible
+ansible-config init --disabled > /etc/ansible/ansible.cfg
+echo"ansible_python_interpreter: /bin/python3">>ansible.cfg/
