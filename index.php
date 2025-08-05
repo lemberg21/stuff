afe771e059da7a67c62ddabbc8f4fe6e571c80f3
Volodymyr Khomyshyn <lemberg_ua@yahoo.com>
2025-08-05 11:48:48 +0300

    Fix bugs

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$ip_server = $_SERVER('SERVER_ADDR);
+
+echo "<h1>Hello form lemberg</h1><br>";
+echo "Server IP address is: $ip_server", "<br><p>";
+echo "Made by <fromt colour=blue>lemberg_ua";
+?>
